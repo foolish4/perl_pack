fad12467e614495e6bfe1c403606c43da50b0652
Phan Thanh Duy <zyverok@gmail.com>
2026-08-06 23:03:30 +0700

    man, this is a blast, adding thicc line!

--- rope.pl
+++ rope.pl
@@ -7,8 +7,10 @@ use OpenGL::GLFW qw(:all);
 use Time::HiRes qw(usleep);
 
 #configuration
+my $PI=3.14159;
 my $WINDOW_WIDTH=800;
 my $WINDOW_HEIGHT=600;
+my $CIRCLE_RESOLUTION=30;
 my $FPS=60.0;
 my $DELAY=1000.0/$FPS;
 my $DELTA_TIME_SEC=1.0/$FPS;
@@ -36,15 +38,27 @@ sub make_vector2{
 		y=>$y
 	};
 }
-sub fill_triangle{
+sub simp_immediate_triangle{
 	my ($v2a,$v2b,$v2c)=@_;
-	my $ax=mod_x($v2a->{x});
-	my $bx=mod_x($v2b->{x});
-	my $cx=mod_x($v2c->{x});
-	my $ay=mod_y($v2a->{y});
-	my $by=mod_y($v2b->{y});
-	my $cy=mod_y($v2c->{y});
-	#bot-left -> bot-right -> top
+	
+	my $ax=$v2a->{x};
+	my $bx=$v2b->{x};
+	my $cx=$v2c->{x};
+	my $ay=$v2a->{y};
+	my $by=$v2b->{y};
+	my $cy=$v2c->{y};
+
+	$ay=$WINDOW_HEIGHT-$ay;
+	$by=$WINDOW_HEIGHT-$by;
+	$cy=$WINDOW_HEIGHT-$cy;
+
+	$ax=mod_x($ax);
+	$bx=mod_x($bx);
+	$cx=mod_x($cx);
+	$ay=mod_y($ay);
+	$by=mod_y($by);
+	$cy=mod_y($cy);
+
 	glBegin(GL_TRIANGLES);
 	{
 		glVertex2f($ax,$ay);
@@ -53,27 +67,70 @@ sub fill_triangle{
 	}
 	glEnd();
 }
-sub simp_immediate_triangle{
-	my ($v2a,$v2b,$v2c)=@_;
-	my $ax=mod_x($v2a->{x});
-	my $bx=mod_x($v2b->{x});
-	my $cx=mod_x($v2c->{x});
-	my $ay=mod_y($v2a->{y});
-	my $by=mod_y($v2b->{y});
-	my $cy=mod_y($v2c->{y});
+sub immediate_circle{
+	my ($center,$radius)=@_;
 
-	#f-flipped
-	my $fay=-$ay;
-	my $fby=-$by;
-	my $fcy=-$cy;
+	my $STEP_ANGLE=(2*$PI)/$CIRCLE_RESOLUTION;
 
-	glBegin(GL_TRIANGLES);
-	{
-		glVertex2f($ax,$fay);
-		glVertex2f($bx,$fby);
-		glVertex2f($cx,$fcy);
+	for(my $i=0;$i<$CIRCLE_RESOLUTION;$i++){
+		my $p0=$center;
+		
+		my $foo_p1=make_vector2(cos($STEP_ANGLE*$i),sin($STEP_ANGLE*$i));
+		$foo_p1->{x}*=$radius;
+		$foo_p1->{y}*=$radius;
+		$foo_p1->{x}+=$center->{x};
+		$foo_p1->{y}+=$center->{y};
+		my $p1=make_vector2($foo_p1->{x},$foo_p1->{y});
+
+		my $foo_p2=make_vector2(cos($STEP_ANGLE*($i+1)),sin($STEP_ANGLE*($i+1)));
+		$foo_p2->{x}*=$radius;
+		$foo_p2->{y}*=$radius;
+		$foo_p2->{x}+=$center->{x};
+		$foo_p2->{y}+=$center->{y};
+		my $p2=make_vector2($foo_p2->{x},$foo_p2->{y});
+
+		simp_immediate_triangle($p0,$p1,$p2);
 	}
-	glEnd();
+}
+sub simp_immediate_quad{
+	my ($p0,$p1,$p2,$p3)=@_;
+	simp_immediate_triangle($p0,$p1,$p2);
+	simp_immediate_triangle($p0,$p2,$p3);
+}
+sub immediate_thicc_line{
+	my ($p0,$p1,$t)=@_;
+
+	my $v1=make_vector2(0,0);
+	$v1->{x}=$p1->{x}-$p0->{x};
+	$v1->{y}=$p1->{y}-$p0->{y};
+
+	my $v2=make_vector2(-$v1->{y},$v1->{x});
+	my $v2l=sqrt($v2->{x}**2+$v2->{y}**2);
+	my $epsilon=0.000001;
+	if($v2l<$epsilon){
+		return;
+	}
+	$v2->{x}/=$v2l;
+	$v2->{y}/=$v2l;
+
+	#fi-finish
+	my $fi1=make_vector2(0,0);
+	$fi1->{x}=$p0->{x}+$v2->{x}*($t/2);
+	$fi1->{y}=$p0->{y}+$v2->{y}*($t/2);
+
+	my $fi2=make_vector2(0,0);
+	$fi2->{x}=$p0->{x}-$v2->{x}*($t/2);
+	$fi2->{y}=$p0->{y}-$v2->{y}*($t/2);
+
+	my $fi3=make_vector2(0,0);
+	$fi3->{x}=$p1->{x}-$v2->{x}*($t/2);
+	$fi3->{y}=$p1->{y}-$v2->{y}*($t/2);
+
+	my $fi4=make_vector2(0,0);
+	$fi4->{x}=$p1->{x}+$v2->{x}*($t/2);
+	$fi4->{y}=$p1->{y}+$v2->{y}*($t/2);
+	
+	simp_immediate_quad($fi1,$fi2,$fi3,$fi4);
 }
 
 sub update{
@@ -85,10 +142,11 @@ sub update{
 	}
 }
 sub render{
-	my $p0=make_vector2(0,0);
-	my $p1=make_vector2($WINDOW_WIDTH,0);
-	my $p2=make_vector2($WINDOW_WIDTH/2,$WINDOW_HEIGHT);
-	simp_immediate_triangle($p0,$p1,$p2);
+	my $pad=100;
+	glColor3f(1.0,0.0,0.0);
+	immediate_thicc_line(make_vector2($pad,$pad),make_vector2($WINDOW_WIDTH-$pad,$WINDOW_HEIGHT-$pad),20);
+
+	#STOP AT HERE: 53:45
 }
 sub main{
 	glfwInit();
